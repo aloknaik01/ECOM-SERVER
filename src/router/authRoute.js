dcd2846bde5fc567d5017c48b1e676b9229cc6b1
aloknaik <aloknaik016261@gmail.com>
2025-09-20 23:07:21 +0530

    feat(auth): add logout route

--- src/router/authRoute.js
+++ src/router/authRoute.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { getUser, login, register } from "../controller/authController.js";
+import {
+  getUser,
+  login,
+  logout,
+  register,
+} from "../controller/authController.js";
 import { isAuth } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -7,4 +12,5 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/getme", isAuth, getUser);
+router.get("/logout", isAuth, logout);
 export default router;
