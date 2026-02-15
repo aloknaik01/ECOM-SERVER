90dd2467dcb6ea9786d234bdc77c4e3ab50e2923
aloknaik01 <aloknaik016261@gmail.com>
2026-02-16 01:34:16 +0530

    feat: Complete database schema for marketplace features

--- src/utils/createTables.js
+++ src/utils/createTables.js
@@ -1,22 +1,35 @@
+import { createWishlistTable } from "../models/wishlistTable.js";
+import { createCouponsTable } from "../models/couponsTable.js";
+import { createCouponUsageTable } from "../models/couponUsageTable.js";
+import { createProductVariantsTable } from "../models/productVariantsTable.js";
+import { createVendorsTable } from "../models/vendorsTable.js";
+import { createVendorPayoutsTable } from "../models/vendorPayoutsTable.js";
+import { createVendorSalesTable } from "../models/vendorSalesTable.js";
+import { addVendorToProducts } from "../models/addVendorToProducts.js";
 import { createUserTable } from "../models/userTable.js";
-import { createOrderItemTable } from "../models/orderItemsTable.js";
 import { createOrdersTable } from "../models/ordersTable.js";
-import { createPaymentsTable } from "../models/paymentsTable.js";
+import { createOrderItemTable } from "../models/orderItemsTable.js";
 import { createProductReviewsTable } from "../models/productReviewsTable.js";
 import { createProductsTable } from "../models/productTable.js";
-import { createShippingInfoTable } from "../models/shippinginfoTable.js";
+import { createShippingInfoTable } from "../models/shippingInfoTable.js";
+import { createPaymentsTable } from "../models/paymentsTable.js";
 
-export const createTables = async () => {
-  try {
-    await createUserTable();
-    await createProductsTable();
-    await createProductReviewsTable();
-    await createOrdersTable();
-    await createOrderItemTable();
-    await createShippingInfoTable();
-    await createPaymentsTable();
-    console.log("All Tables Created Successfully.");
-  } catch (error) {
-    console.error("Error creating tables:", error);
-  }
-};
+export async function createTables() {
+  await createUserTable();
+  await createProductsTable();
+  await createOrdersTable();
+  await createOrderItemTable();
+  await createShippingInfoTable();
+  await createPaymentsTable();
+  await createProductReviewsTable();
+  
+  // NEW TABLES
+  await createWishlistTable();
+  await createCouponsTable();
+  await createCouponUsageTable();
+  await createProductVariantsTable();
+  await createVendorsTable();
+  await createVendorPayoutsTable();
+  await createVendorSalesTable();
+  await addVendorToProducts();  
+}
